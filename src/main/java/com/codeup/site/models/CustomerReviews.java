5153ef52c688fd8ddc11b8015c48aa1671d36802
Travis Gomez <frank.travis.gomez@gmail.com>
2023-09-02 21:53:15 -0500

    constructor with getters and setters for DigitalAsset

--- src/main/java/com/codeup/site/models/CustomerReviews.java
+++ src/main/java/com/codeup/site/models/CustomerReviews.java
@@ -1,86 +1,86 @@
-package com.codeup.site.models;
-
-import jakarta.persistence.*;
-
-@Entity
-@Table(name = "cust_review")
-public class CustomerReviews {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
-    private int id;
-
-    @Column(name = "stars", nullable = false, length = 5)
-    @Enumerated(EnumType.STRING)
-    private Stars stars;
-
-    @Column(name = "comment")
-    private String comment;
-
-    @ManyToOne
-    @JoinColumn(name = "item_id", nullable = false)
-    private Items item;
-
-    @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
-    private Users user;
-
-    /*////////////////////////////////////////////////////////////////
-    CONSTRUCTORS
-    ////////////////////////////////////////////////////////////////*/
-
-    public CustomerReviews() {}
-
-    public CustomerReviews(Stars stars, String comment, Items item, Users user) {
-        this.stars = stars;
-        this.comment = comment;
-        this.item = item;
-        this.user = user;
-    }
-
-    public CustomerReviews(int id, Stars stars, String comment, Items item, Users user) {
-        this.id = id;
-        this.stars = stars;
-        this.comment = comment;
-        this.item = item;
-        this.user = user;
-    }
-
-    /*////////////////////////////////////////////////////////////////
-    GETTERS & SETTERS
-    ////////////////////////////////////////////////////////////////*/
-
-    public int getId() {return id;}
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Stars getStars() {
-        return stars;
-    }
-    public void setStars(Stars stars) {
-        this.stars = stars;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    public Items getTask() {
-        return item;
-    }
-    public void setTask(Items task) {
-        this.item = item;
-    }
-
-    public Users getUsers() {
-        return user;
-    }
-    public void setUsers(Users user) {
-        this.user = user;
-    }
-}
+//package com.codeup.site.models;
+//
+//import jakarta.persistence.*;
+//
+//@Entity
+//@Table(name = "cust_review")
+//public class CustomerReviews {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "id", nullable = false)
+//    private int id;
+//
+//    @Column(name = "stars", nullable = false, length = 5)
+//    @Enumerated(EnumType.STRING)
+//    private Stars stars;
+//
+//    @Column(name = "comment")
+//    private String comment;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "item_id", nullable = false)
+//    private Items item;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "user_id", nullable = false)
+//    private Users user;
+//
+//    /*////////////////////////////////////////////////////////////////
+//    CONSTRUCTORS
+//    ////////////////////////////////////////////////////////////////*/
+//
+//    public CustomerReviews() {}
+//
+//    public CustomerReviews(Stars stars, String comment, Items item, Users user) {
+//        this.stars = stars;
+//        this.comment = comment;
+//        this.item = item;
+//        this.user = user;
+//    }
+//
+//    public CustomerReviews(int id, Stars stars, String comment, Items item, Users user) {
+//        this.id = id;
+//        this.stars = stars;
+//        this.comment = comment;
+//        this.item = item;
+//        this.user = user;
+//    }
+//
+//    /*////////////////////////////////////////////////////////////////
+//    GETTERS & SETTERS
+//    ////////////////////////////////////////////////////////////////*/
+//
+//    public int getId() {return id;}
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public Stars getStars() {
+//        return stars;
+//    }
+//    public void setStars(Stars stars) {
+//        this.stars = stars;
+//    }
+//
+//    public String getComment() {
+//        return comment;
+//    }
+//    public void setComment(String comment) {
+//        this.comment = comment;
+//    }
+//
+//    public Items getTask() {
+//        return item;
+//    }
+//    public void setTask(Items task) {
+//        this.item = item;
+//    }
+//
+//    public Users getUsers() {
+//        return user;
+//    }
+//    public void setUsers(Users user) {
+//        this.user = user;
+//    }
+//}
--- src/main/java/com/codeup/site/models/DigitalAsset.java
+++ src/main/java/com/codeup/site/models/DigitalAsset.java
@@ -28,4 +28,80 @@ public class DigitalAsset {
 
     @Column(name = "is_active", nullable = false)
     private boolean isActive = true;
+
+    public DigitalAsset() {
+
+    }
+
+    public DigitalAsset(
+            String assetName,
+            String description,
+            BigDecimal price,
+            String filePath,
+            String downloadLink,
+            boolean isActive
+    ) {
+        this.assetName = assetName;
+        this.description = description;
+        this.price = price;
+        this.filePath = filePath;
+        this.downloadLink = downloadLink;
+        this.isActive = isActive;
+    }
+
+    public int getAssetId() {
+        return assetId;
+    }
+
+    public void setAssetId(int assetId) {
+        this.assetId = assetId;
+    }
+
+    public String getAssetName() {
+        return assetName;
+    }
+
+    public void setAssetName(String assetName) {
+        this.assetName = assetName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getDownloadLink() {
+        return downloadLink;
+    }
+
+    public void setDownloadLink(String downloadLink) {
+        this.downloadLink = downloadLink;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean isActive) {
+        this.isActive = isActive;
+    }
 }
