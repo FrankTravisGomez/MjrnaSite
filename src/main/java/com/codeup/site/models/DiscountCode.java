3757408183505436518ea9245c6a4cc4c1dd6e1c
Travis Gomez <frank.travis.gomez@gmail.com>
2023-09-04 21:56:32 -0500

    constructor and getter setters for DiscountCode

--- src/main/java/com/codeup/site/models/DiscountCode.java
+++ src/main/java/com/codeup/site/models/DiscountCode.java
@@ -19,4 +19,46 @@ public class DiscountCode {
 
     @Column(name = "expiration_date", nullable = false)
     private LocalDate expirationDate;
+
+    public DiscountCode() {
+
+    }
+
+    public DiscountCode(String code, BigDecimal discountPercentage, LocalDate expirationDate) {
+        this.code = code;
+        this.discountPercentage = discountPercentage;
+        this.expirationDate = expirationDate;
+    }
+
+    public int getCodeId() {
+        return codeId;
+    }
+
+    public void setCodeId(int codeId) {
+        this.codeId = codeId;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public BigDecimal getDiscountPercentage() {
+        return discountPercentage;
+    }
+
+    public void setDiscountPercentage(BigDecimal discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
 }
