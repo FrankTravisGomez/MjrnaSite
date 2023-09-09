e578162454721e38807cc7278633e4883db3cf5e
Travis Gomez <frank.travis.gomez@gmail.com>
2023-09-08 22:32:40 -0500

    constructor getter setter for User

--- src/main/java/com/codeup/site/models/User.java
+++ src/main/java/com/codeup/site/models/User.java
@@ -45,4 +45,119 @@ public class User {
 
     @Column(name = "is_admin", nullable = false)
     private boolean isAdmin;
+
+    public User() {
+
+    }
+
+    public User(String firstName, String lastName, String email, String discordName, String vrChatName, boolean emailVerified,
+                boolean receiveUpdates, String passwordHash, String resetToken, Timestamp resetTokenExpiry, boolean isAdmin) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.discordName = discordName;
+        this.vrChatName = vrChatName;
+        this.emailVerified = emailVerified;
+        this.receiveUpdates = receiveUpdates;
+        this.passwordHash = passwordHash;
+        this.resetToken = resetToken;
+        this.resetTokenExpiry = resetTokenExpiry;
+        this.isAdmin = isAdmin;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDiscordName() {
+        return discordName;
+    }
+
+    public void setDiscordName(String discordName) {
+        this.discordName = discordName;
+    }
+
+    public String getVrChatName() {
+        return vrChatName;
+    }
+
+    public void setVrChatName(String vrChatName) {
+        this.vrChatName = vrChatName;
+    }
+
+    public boolean isEmailVerified() {
+        return emailVerified;
+    }
+
+    public void setEmailVerified(boolean emailVerified) {
+        this.emailVerified = emailVerified;
+    }
+
+    public boolean isReceiveUpdates() {
+        return receiveUpdates;
+    }
+
+    public void setReceiveUpdates(boolean receiveUpdates) {
+        this.receiveUpdates = receiveUpdates;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getResetToken() {
+        return resetToken;
+    }
+
+    public void setResetToken(String resetToken) {
+        this.resetToken = resetToken;
+    }
+
+    public Timestamp getResetTokenExpiry() {
+        return resetTokenExpiry;
+    }
+
+    public void setResetTokenExpiry(Timestamp resetTokenExpiry) {
+        this.resetTokenExpiry = resetTokenExpiry;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
 }
