94ebf677e49163325ad9c45cc5c0837f553ec2a5
Travis Gomez <frank.travis.gomez@gmail.com>
2023-09-11 21:32:40 -0500

    contrsuctor getter setter UserDiscountCodeld

--- src/main/java/com/codeup/site/models/UserDiscountCodeId.java
+++ src/main/java/com/codeup/site/models/UserDiscountCodeId.java
@@ -1,10 +1,48 @@
 package com.codeup.site.models;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class UserDiscountCodeId implements Serializable {
     private int userId;
     private int codeId;
 
-    // ... equals and hashCode methods
+    public UserDiscountCodeId() {
+
+    }
+
+    public UserDiscountCodeId(int userId, int codeId) {
+        this.userId = userId;
+        this.codeId = codeId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getCodeId() {
+        return codeId;
+    }
+
+    public void setCodeId(int codeId) {
+        this.codeId = codeId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserDiscountCodeId)) return false;
+        UserDiscountCodeId that = (UserDiscountCodeId) o;
+        return getUserId() == that.getUserId() && getCodeId() == that.getCodeId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUserId(), getCodeId());
+    }
+
 }
