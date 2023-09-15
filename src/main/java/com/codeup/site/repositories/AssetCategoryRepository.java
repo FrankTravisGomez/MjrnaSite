5fdf7405836b16517003225ca53a1d35e7b26859
Travis Gomez <frank.travis.gomez@gmail.com>
2023-09-14 22:54:56 -0500

    making repositories

---
+++ src/main/java/com/codeup/site/repositories/AssetCategoryRepository.java
@@ -0,0 +1,8 @@
+package com.codeup.site.repositories;
+
+import com.codeup.site.models.AssetCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AssetCategoryRepository extends JpaRepository<AssetCategory, Integer> {
+
+}
