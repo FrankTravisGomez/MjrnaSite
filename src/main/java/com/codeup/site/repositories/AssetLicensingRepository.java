e91f61161cb7edcda01810a5a1c1cfa57252cbe8
Travis Gomez <frank.travis.gomez@gmail.com>
2023-09-15 23:12:45 -0500

    new repositorie

---
+++ src/main/java/com/codeup/site/repositories/AssetLicensingRepository.java
@@ -0,0 +1,8 @@
+package com.codeup.site.repositories;
+
+import com.codeup.site.models.AssetLicensing;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AssetLicensingRepository extends JpaRepository<AssetLicensing, Integer> {
+
+}
